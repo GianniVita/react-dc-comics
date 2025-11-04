ce38aac2742698d46bf465e283fdbcd71c3f1b51
giannivitale <giavitale78@gmail.com>
2025-11-04 16:22:52 +0100

    Create a simple nav navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,33 @@
-export default function Hearder(){
+export default function Hearder() {
 
-    return(
-        
+    return (
+
         <>
-        <header className="main-header">
-        <img src="../src/assets/img/dc-logo.png" alt="" />
-
-
-
-        </header>
+            <header className="main-header">
+                <img src="../src/assets/img/dc-logo.png" alt="" />
+                <nav className="main-nav">
+                    <ul>
+                        <li><a href="">CHARACTERS</a></li>
+                        <li><a href="">COMICS</a></li>
+                        <li><a href="">MOVIES</a></li>
+                        <li><a href="">TV</a></li>
+                        <li><a href="">GAMES</a></li>
+                        <li><a href="">COLLECTTIBLES</a></li>
+                        <li><a href="">VIDEOS</a></li>
+                        <li><a href="">FANS</a></li>
+                        <li><a href="">NEWS</a></li>
+                        <li><a href="">SHOP</a></li>
+                    </ul>
+                </nav>
+            </header>
         </>
-        
+
     )
 }
+                        
+
+
+
+
+
+
